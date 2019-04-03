ae4fd0c39f4d60a07ea318a4064f9ee0684d714e
Nikolaj Andersen <andersen.nl@icloud.com>
2019-04-03 17:39:54 +0200

    Initial Commit

---
+++ rspec/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  rspec
+//
+//  Created by Nikolaj Andersen on 03.04.19.
+//  Copyright © 2019 Nikolaj Andersen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
